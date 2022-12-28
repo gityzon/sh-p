a5fc6524f624498ac683a1b2f03bf2264c8c3763
gityzon <109445070+gityzon@users.noreply.github.com>
2022-12-28 15:45:22 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,16 @@
+######请添加环境变量"uuid",否则xray无效
+#删除panindex并重新启动以更新PanIndex.
+#后台地址（默认）：http://ip:port/admin 默认密码：PanIndex 第一次安装后需要进行配置， 请务必修改默认密码
+
+#!/bin/bash
+
+if [ ! -f "panindex" ];then
+  curl -L https://github.com/libsgh/PanIndex/releases/latest/download/PanIndex-linux-amd64.tar.gz -o panindex.tar.gz
+tar -zxvf panindex.tar.gz
+mv PanIndex-linux-amd64 panindex
+rm -f panindex.tar.gz & rm -f LICENSE
+fi
+chmod +x panindex
+
+
+./panindex -port=3000
